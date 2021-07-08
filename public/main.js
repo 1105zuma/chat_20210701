534a1e33a7213062e7f6818bdfd8a8faeae50b4b
Ryotaro Shimizu <220c0492@s.obirin.ac.jp>
2021-07-08 17:24:42 +0900

    jsファイル_分離_修正1

--- public/main.js
+++ public/main.js
@@ -3,12 +3,12 @@ const form = document.getElementByld("form")
 const input =document.getElementByld("msg")
 const chats =document.getElementByld("chats")
 
-form.addEventListener("submit",function(event){
-    socketio.emit("message",input,value)
-    input,value=""
+form.addEventListener('submit',function(event){
+    socketio.emit('message',input,value)
+    input,value=''
     event,preventDefault()
 })
-socketio.on("message",function(msg){
+socketio.on('message',function(msg){
     const li = document.createElement("li")
     li.append(msg)
     chats.append(li)
